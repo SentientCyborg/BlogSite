d5d90a9e75ebc3270b823f629c876426d1662a29
J. Freeman <85462620+SentientCyborg@users.noreply.github.com>
2024-05-25 17:09:00 -0700

    Update logic to use data from database

--- database/migrations/2024_05_25_212728_create_posts_table.php
+++ database/migrations/2024_05_25_212728_create_posts_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('subtitle');
+            $table->text('text');
+            $table->string('author');
             $table->timestamps();
         });
     }
