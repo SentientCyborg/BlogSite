5cc0075384e8ad59ca665f8fdf5898b0b032e66a
J. Freeman <85462620+SentientCyborg@users.noreply.github.com>
2024-08-26 19:00:33 -0700

    Remove comment

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,28 +8,13 @@ use App\Models\Post;
 class PostController extends Controller
 {   
 
-    // public function showPosts(): View {
-    //     return view('index', ['posts' => Post::all()]);
-    // }
-
-<<<<<<< HEAD
-    // public function showOnePost($id): View {
-    //     $post = Post::find($id);
-    //     return view('post', ['post' => $post]);
-    // }
-    
-=======
-    }
-
     public function showPosts(): View {
-        return view('posts.index', ['posts' => $this->posts['testPosts']]);
+        return view('index', ['posts' => Post::all()]);
     }
 
     public function showOnePost($id): View {
-        $post = Arr::first($this->posts['testPosts'], fn($post) => $post['id'] == $id);
-        return view('posts.show', ['post' => $post]);
+        $post = Post::find($id);
+        return view('post', ['post' => $post]);
     }
-
-    //TODO: Add create post function
->>>>>>> ddf235cba6a7dacad38ef3b238d7a19488222eeb
+    
 }
