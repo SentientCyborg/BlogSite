f8b598b1ef0a80dc8d0cb5ad628c5812a37d29a6
J. Freeman <85462620+SentientCyborg@users.noreply.github.com>
2024-08-29 19:11:26 -0700

    Remove TODO

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-//TODO: Create dedicated post routing instead of using the home route
 Route::get('/post', [PostController::class, 'showPosts']);
 Route::get('/post/{id}', [PostController::class, 'showOnePost']);
 Route::get('/post/create', [PostController::class, 'createPost']);
