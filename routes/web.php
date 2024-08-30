70ee214dd901ca32a1227f9fa26a7ae29625da4f
J. Freeman <85462620+SentientCyborg@users.noreply.github.com>
2024-08-29 20:20:34 -0700

    Update name of route that views multiple posts

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,12 @@
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/post', [PostController::class, 'showPosts']);
+
+Route::get('/', function() {
+    return view('home');
+});
+
+Route::get('/posts', [PostController::class, 'showPosts']);
 Route::get('/post/{id}', [PostController::class, 'showOnePost']);
 Route::get('/post/create', [PostController::class, 'createPost']);
 
