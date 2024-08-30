c2ddb006096ce79be5fcae09f9af5072dac165fc
J. Freeman <85462620+SentientCyborg@users.noreply.github.com>
2024-08-29 20:17:14 -0700

    Add link to create post page

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,6 +7,9 @@
     <div class="container px-4 px-lg-5">
         <div class="row gx-4 gx-lg-5 justify-content-center">
             <div class="col-md-10 col-lg-8 col-xl-7">
+
+                {{-- TODO: Put this link someplace else --}}
+                <a class="btn btn-primary" href="/post/create" role="button">Create Post</a>
                 
                 @foreach ($posts as $post)
                     
