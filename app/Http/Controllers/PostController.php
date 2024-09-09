6a9111bcd64ea28c993caacd38927a5a3ecd2828
J. Freeman <85462620+SentientCyborg@users.noreply.github.com>
2024-09-08 20:47:13 -0700

    Add 'store' function to test form submission

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\View\View;
+use Illuminate\Http\Request;
 use App\Models\Post;
+use Illuminate\Http\RedirectResponse;
+
 
 class PostController extends Controller
 {   
@@ -17,8 +20,18 @@ class PostController extends Controller
         return view('posts.show', ['post' => $post]);
     }
 
-    public function createPost(): View {
+    public function newPost(): View {
         return View('posts.create');
     }
+
+    // public function store() : RedirectResponse {
+    //     ~ Code goes here. ~
+    //     return redirect('/posts');
+    // }
     
+    //Test function
+    public function store() {
+        // dd('Spam and Eggs');
+        dd(request()->all()); //TODO: This is not working. The request is only getting the CSRF token.
+    }
 }
