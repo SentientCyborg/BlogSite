99e7a282ff4d200c359d0a4a8c985c60f9cae18d
J. Freeman <85462620+SentientCyborg@users.noreply.github.com>
2024-09-08 20:41:45 -0700

    Update routes to add post request to '/posts' route

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,8 @@ Route::get('/', function() {
 });
 
 Route::get('/posts', [PostController::class, 'showPosts']);
-Route::get('/post/create', [PostController::class, 'createPost']);
+Route::post('/posts', [PostController::class, 'store']);
+Route::get('/post/create', [PostController::class, 'newPost']);
 Route::get('/post/{id}', [PostController::class, 'showOnePost']);
 
 Route::get('/about', function () {
