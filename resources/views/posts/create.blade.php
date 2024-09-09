ecffe2b4f6b975d80341b52537ece86a076cbfc5
J. Freeman <85462620+SentientCyborg@users.noreply.github.com>
2024-09-08 20:45:57 -0700

    Add form to allow creation of new posts

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -9,7 +9,7 @@
             <div class="col-md-10 col-lg-8 col-xl-7 mb-3">
                 
                 <h1>TODO: Update this page.</h1>
-                <form method="POST" action="">
+                <form method="POST" action="{{ url('/posts') }}">
                     @csrf
                     <div class="mb-3">
                         <label for="FormControlInput1" class="form-label">Post Title</label>
@@ -24,7 +24,9 @@
                         <textarea class="form-control" id="exampleFormControlTextarea1" rows="5"></textarea>
                       </div>
                                       
-                      <button type="submit" class="btn btn-primary">Submit</button>
+                      {{-- <button type="submit" class="btn btn-primary">Test Submit</button> --}}
+                      <x-button>Submit</x-button>
+                    </div>
                 </form>
 
             </div>
