2855d05764cf8dd0270c67e6560a44d01654f905
J. Freeman <85462620+SentientCyborg@users.noreply.github.com>
2024-09-15 15:16:11 -0700

    Add error notices

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -11,20 +11,31 @@
                 <h1>TODO: Update this page.</h1>
                 <form method="POST" action="{{ url('/posts') }}">
                     @csrf
+
                     <div class="mb-3">
-                        <label for="title" class="form-label">Post Title</label>
-                        <input type="text" name="title" class="form-control" id="FormControlInput1" placeholder="Enter a title" required>
-                      </div>
-                      <div class="mb-3">
-                        <label for="subtitle" class="form-label">Subtitle</label>
-                        <input type="text" name="subtitle" class="form-control" id="FormControlInput2" placeholder="Enter a short post description." required>
-                      </div> 
-                      <div class="mb-3">
-                        <label for="text" class="form-label">Blog Entry</label>
-                        <textarea name="text" class="form-control" id="exampleFormControlTextarea1" rows="5" required></textarea>
-                      </div>
-                                      
-                      {{-- <button type="submit" class="btn btn-primary">Test Submit</button> --}}
+                      <label for="title" class="form-label">Post Title</label>
+                      <input type="text" name="title" class="form-control" id="FormControlInput1" placeholder="Enter a title" required>
+                      @error("title")
+                        <x-error> {{$message}} </x-error>
+                      @enderror
+                    </div>
+
+                    <div class="mb-3">
+                      <label for="subtitle" class="form-label">Subtitle</label>
+                      <input type="text" name="subtitle" class="form-control" id="FormControlInput2" placeholder="Enter a short post description." required>
+                      @error("subtitle")
+                        <x-error> {{$message}} </x-error>
+                      @enderror
+                    </div> 
+
+                    <div class="mb-3">
+                      <label for="text" class="form-label">Blog Entry</label>
+                      <textarea name="text" class="form-control" id="exampleFormControlTextarea1" rows="5" required></textarea>
+                      @error("text")
+                        <x-error> {{$message}} </x-error>
+                      @enderror
+                    </div>  
+                                                    
                       <x-button>Submit</x-button>
                     </div>
                 </form>
