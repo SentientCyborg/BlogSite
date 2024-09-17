ab249fafef2c6bad0363cad5040d024d9898122a
J. Freeman <85462620+SentientCyborg@users.noreply.github.com>
2024-09-16 21:11:57 -0700

    Restore deleted route

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ Route::get('/', function() {
 Route::get('/posts', [PostController::class, 'showPosts']);
 Route::post('/posts', [PostController::class, 'store']);
 Route::get('/post/create', [PostController::class, 'newPost']);
+Route::get('/post/{id}', [PostController::class, 'showOnePost']);
 Route::patch('/post/{id}', [PostController::class, 'updatePost']);
 Route::delete('/post/{id}', [PostController::class, 'deletePost']);
 Route::get('/post/{id}/edit', [PostController::class, 'editPost']);
