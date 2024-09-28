2bbd88feee696eb6218971501acf3b0613bb1602
J. Freeman <85462620+SentientCyborg@users.noreply.github.com>
2024-09-28 12:34:37 -0700

    Update routes to support login and auth

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,25 @@
 <?php
 
-use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\SessionController;
 
-
+// Nav Home
 Route::view('/', 'home');
 
+// User Posts Routing
 Route::resource('posts', PostController::class);
 
+// Auth
+Route::get('/register', [RegisteredUserController::class, 'create']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
+
+// Login
+Route::get('/login', [SessionController::class, 'create']);
+Route::post('/login', [SessionController::class, 'store']);
+
+// Nav Other Routes
 Route::view('/about', 'about');
 Route::view('/contact', 'contact');
 
