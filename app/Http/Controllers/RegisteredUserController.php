597aeff88aac2ebd9f52721f4f4bbb2ec0c371a4
J. Freeman <85462620+SentientCyborg@users.noreply.github.com>
2024-09-28 12:32:19 -0700

    Add functions with placeholder logic

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\View\View;
 use Illuminate\Http\Request;
 
 class RegisteredUserController extends Controller
 {
-    //
+    public function create() : View {
+        return view('auth.register');
+    } 
+
+    public function store(): {
+        // TODO: update logic
+        dd(request()->all()); 
+    }
 }
--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\View\View;
 use Illuminate\Http\Request;
 
 class SessionController extends Controller
 {
-    //
+    public function create () : View {
+        return view('auth.login');
+    }
+
+    public function store () : {
+        // TODO: update logic
+        dd(request()->all());
+    }
 }
