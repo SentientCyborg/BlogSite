242190ce3d791d0d6378204937bca3777dc975be
J. Freeman <85462620+SentientCyborg@users.noreply.github.com>
2024-09-28 12:24:30 -0700

    Add views to support auth

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,6 @@
+<x-layout bgImg="contact-bg.jpg">
+    
+    <x-slot:heading>Login</x-slot:heading>
+    <x-slot:subHeading>Please Login</x-slot:subHeading>
+    <p>Login Placeholder</p>
+</x-layout>
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,6 @@
+<x-layout bgImg="contact-bg.jpg">
+    
+    <x-slot:heading>Register</x-slot:heading>
+    <x-slot:subHeading>Please Register</x-slot:subHeading>
+    <p>Register Placeholder</p>
+</x-layout>
