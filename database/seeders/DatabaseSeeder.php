ae2f2e0dde5426ec921abf96509eafd0547bc407
J. Freeman <85462620+SentientCyborg@users.noreply.github.com>
2024-10-13 12:07:13 -0700

    Update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,8 +15,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([
-            UserSeeder::class,
-        ]);
+        $users = User::factory(20)->create();
+
+        Post::factory(100)
+            ->recycle($users)
+            ->create();
+        
+        // $this->call([
+        //     UserSeeder::class,
+        //     PostSeeder::class,
+        // ]);
     }
 }
