6e387040fdd3cc343af59954054aedb31ae983c5
J. Freeman <85462620+SentientCyborg@users.noreply.github.com>
2024-10-20 17:37:20 -0700

    Add logic for registering users

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -2,8 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\View\View;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Validation\Rules\Password;
+use PhpParser\Node\Expr\FuncCall;
 
 class RegisteredUserController extends Controller
 {
@@ -11,8 +17,20 @@ class RegisteredUserController extends Controller
         return view('auth.register');
     } 
 
-    public function store(): never {
-        // TODO: update logic
-        dd(request()->all()); 
+    public function store(): RedirectResponse {
+
+        $attributes = request()->validate([
+            'first_name' => ['required','string', 'max:254'],
+            'last_name'=> ['required','string', 'max:254'],
+            'email'=> ['required','email', 'max:254'],
+            'password'=> ['required', Password::min(size: 12), 'max: 254', 'confirmed'],
+        ]);
+
+        $user = User::create($attributes);
+
+        Auth::login($user);
+
+        return Redirect::route('posts.index');
     }
+
 }
