e61d4b8bb83cc330d1d7999e2e395c3424bb72b3
J. Freeman <85462620+SentientCyborg@users.noreply.github.com>
2024-10-21 18:17:45 -0700

    Update login/logout implementation.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -33,9 +33,22 @@
                         <li class="nav-item"><x-nav-link href="/posts">View Posts</x-nav-link></li>
                         <li class="nav-item"><x-nav-link href="/about">About</x-nav-link></li>
                         <li class="nav-item"><x-nav-link href="/contact">Contact</x-nav-link></li>
-                        {{-- TODO: Hide these links for logged in users --}}
-                        <li class="nav-item"><x-nav-link href="/login">Login</x-nav-link></li>
-                        <li class="nav-item"><x-nav-link href="/register">Register</x-nav-link></li>
+                        
+                        @guest
+                            <li class="nav-item"><x-nav-link href="/login">Login</x-nav-link></li>
+                            <li class="nav-item"><x-nav-link href="/register">Register</x-nav-link></li>
+                        @endguest
+
+                        @auth
+                            <form method="POST" action="/logout">
+                                @csrf
+                                <x-form-button> 
+                                    {{-- TODO: Align Button --}}
+                                    Log Out
+                                </x-form-button>
+                            </form>
+                        @endauth
+
                     </ul>
                 </div>
             </div>
